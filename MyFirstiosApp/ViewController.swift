b2d13812ed6d2e1417545cbbfedd809409782c12
VicWang <ctwang1223@gmail.com>
2019-07-11 20:24:59 +0800

    Initial Commit

---
+++ MyFirstiosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstiosApp
+//
+//  Created by vic on 2019/7/11.
+//  Copyright © 2019 CHENG-TE WANG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
